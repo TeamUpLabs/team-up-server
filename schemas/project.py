0bd63be17766551cdd8a634af77f5925ba179cdb
LeeSeokmin <dltjrals13@naver.com>
2025-04-25 10:27:56 +0900

    Update participationRequest field in Project schema to accept a list of integers instead of Member objects, streamlining participation request handling.

--- schemas/project.py
+++ schemas/project.py
@@ -30,7 +30,7 @@ class Project(ProjectBase):
     milestones: Optional[List[MileStone]] = []
     leader: Optional[Member] = None
     manager: Optional[List[Member]] = []
-    participationRequest: Optional[List[Member]] = []
+    participationRequest: Optional[List[int]] = []
     class Config:
         from_attributes = True
         
