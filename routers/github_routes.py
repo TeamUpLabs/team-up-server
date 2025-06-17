4fcb7aa6bfd4d20d74bd611b07ab4fb07d33cab8
LeeSeokmin <dltjrals13@naver.com>
2025-06-17 14:11:04 +0900

    Refactor GitHub routes to support project-specific repository management
    - Updated the GitHub routes to change the prefix from `/github` to `/project`, allowing for project-specific operations.
    - Enhanced the `create_org_repo` function to associate the created GitHub repository with a project by updating the `github_repo_url` field in the Project model.
    - Added a new `delete_org_repo` function to remove the GitHub repository link from the project.
    - Improved error handling to ensure proper responses when projects are not found or when exceptions occur during repository operations.

--- routers/github_routes.py
+++ routers/github_routes.py
@@ -1,16 +1,20 @@
+from stat import SF_DATALESS
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from database import SessionLocal
 import requests
 import logging
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
+from crud.project import get_project
+from utils.sse_manager import project_sse_manager
+import json
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 router = APIRouter(
-    prefix="/github",
+    prefix="/project",
     tags=["github"]
 )
 
@@ -24,19 +28,56 @@ def get_db():
 class CreateOrgRepo(BaseModel):
     github_access_token: str
     repo_name: str
+    repo_description: str
+    isPrivate: bool
     
-@router.post("/org/create-repo")
-def create_org_repo(repo: CreateOrgRepo):
+@router.post("/{project_id}/github/org/create-repo")
+async def create_org_repo(project_id: str, repo: CreateOrgRepo, db: SessionLocal = Depends(get_db)):
     github_token = repo.github_access_token
     headers = {"Authorization": f"token {github_token}", "Accept": "application/vnd.github+json"}
     payload = {
         "name": repo.repo_name,
-        "private": True,
+        "description": repo.repo_description,
         "auto_init": True,
+        "private": repo.isPrivate
     }
     org = "TeamUpLabs"
     res = requests.post(f"https://api.github.com/orgs/{org}/repos", json=payload, headers=headers)
     if res.status_code == 201:
-        return {"success": True, "repo": res.json()}
+      try:
+        project = get_project(db, project_id)
+        if project:
+          project.github_repo_url = res.json()["html_url"]
+          db.add(project)
+          db.commit()
+          
+          await project_sse_manager.send_event(
+                project_id,
+                json.dumps(project_sse_manager.convert_to_dict(project))
+            )
+          return {"success": True, "repo": res.json()}
+        else:
+          raise HTTPException(status_code=404, detail="Project not found")
+      except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     else:
         raise HTTPException(status_code=res.status_code, detail=res.json())
+  
+@router.delete("/{project_id}/github/org/delete-repo")
+async def delete_org_repo(project_id: str, db: SessionLocal = Depends(get_db)):
+  try:
+    project = get_project(db, project_id)
+    if project:
+      project.github_repo_url = None
+      db.add(project)
+      db.commit()
+      
+      await project_sse_manager.send_event(
+            project_id,
+            json.dumps(project_sse_manager.convert_to_dict(project))
+        )
+      return {"success": True}
+    else:
+      raise HTTPException(status_code=404, detail="Project not found")
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
